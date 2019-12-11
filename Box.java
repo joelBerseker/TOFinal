acdb790d79deb1df46cfa4852dfd9531fd29536f
log4x <achavezsa@unsa.edu.pe>
2019-12-11 09:54:30 -0500

    cambios para el barco xd

--- Box.java
+++ Box.java
@@ -1,5 +1,7 @@
 public class Box{
     private Object dataBox;
+    private int x,y;
+    
     //marked para x, filled para y
     //el eje x siempre lleva vocal de la A a la J
     //el eje y siempre lleva numero del 1 al 10
@@ -7,14 +9,34 @@ public class Box{
     public Box(Object Data){
         this.dataBox=Data;
     }
+    public Box(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
     public Object getData(){
         return this.dataBox;
     }
     public void setData(Object Data){
         this.dataBox=Data;
     }
-     
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public int getIndex(){
+        return x*10+y;
+    } 
     public String toString(){
         return dataBox.toString();
     }
+
+    
 }
---
+++ pruebas2.java
@@ -0,0 +1,10 @@
+public class pruebas2{
+    public static void main(String[] args) {
+        Box b1=new Box(0,0);
+        Box b2=new Box(1,0);
+        Box b3=new Box(0,2);
+        Box b4=new Box(0,3);
+        Ship s1=new Ship(b1,b2);
+        
+    }
+}
