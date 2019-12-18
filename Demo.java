d9f8a0260537b64e15b615acb749bb9435e3eb7d
joelBerseker <jatamaria@unsa.edu.pe>
2019-12-18 11:01:54 -0500

    a

--- Demo.java
+++ Demo.java
@@ -1,7 +1,8 @@
 
 public class Demo{
     public static void main(String[]args) {
-		Game game = new GameBattleShips();
+		//Game game = new GameBattleShips();
+		Game game = new GameTresEnRaya();
 		PrintBoard printL = new PrintBoard(game);
 		printL.setVisible(true);
 	}
--- PrintBoard.java
+++ PrintBoard.java
@@ -17,35 +17,33 @@ public class PrintBoard extends JFrame implements ActionListener{
 		setTitle(this.game.getBoard().getGameName());
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-        contents(this.game.getBoard());
+        
         setLayout(new  BorderLayout());
-        add(central,BorderLayout.CENTER);
-        add(panelSuperior,BorderLayout.NORTH);
-        add(panelInferior,BorderLayout.SOUTH);
-        add(panelIzquierdo,BorderLayout.WEST);
-        add(panelDerecho,BorderLayout.EAST);
-        setVisible(true);
-        for(int i=0;i<game.getBoard().getRows();i++){
-            for(int j=0;j<game.getBoard().getColumns();j++){
-                Image img= new ImageIcon("image\\x.png").getImage();
-                ImageIcon img2=new ImageIcon(img.getScaledInstance(78, 124, Image.SCALE_SMOOTH));
-                b[i][j].setIcon(img2);
-            }
-        }
-    }
-    public void contents(Board board) {
+        
         central= new JPanel();
         panelSuperior= new JPanel();
         panelInferior= new JPanel();
         panelIzquierdo= new JPanel();
         panelDerecho=new JPanel();
-        if(board.getGameName().equalsIgnoreCase("Tres en Raya")){
-            contentsTreeInLine(board);
-        }else if(board.getGameName().equalsIgnoreCase("Botella borracha")){
-            contentsDrunkBottle(board);
+        contents();
+        add(central,BorderLayout.CENTER);
+        /*add(panelSuperior,BorderLayout.NORTH);
+        add(panelInferior,BorderLayout.SOUTH);
+        add(panelIzquierdo,BorderLayout.WEST);
+        add(panelDerecho,BorderLayout.EAST);
+        */
+        setVisible(true);
+        
+    }
+    public void contents() {
+        if(this.game.getBoard().getGameName().equalsIgnoreCase("Tres en Raya")){
+            contentsTreeInLine(this.game.getBoard());
+        }else if(this.game.getBoard().getGameName().equalsIgnoreCase("Botella borracha")){
+            contentsDrunkBottle(this.game.getBoard());
         }else{
-            contentsBatleShips(board);
+            contentsBatleShips();
         }
+        /*
         panelSuperior.setLayout( new FlowLayout(FlowLayout.CENTER));
         panelSuperior.add(new JLabel("MI GAME"));
         panelInferior.setLayout( new FlowLayout(FlowLayout.CENTER));
@@ -59,35 +57,58 @@ public class PrintBoard extends JFrame implements ActionListener{
         panelDerecho.setLayout(new GridLayout(3,1));
         panelDerecho.add(new JTextField("Holi"));
         panelDerecho.add(new JButton("Holi"));
+        */
     }
     void contentsTreeInLine(Board board){
-        central.setSize(200,200);
-        central.setLayout(new GridLayout(3,3));
+        central.setLayout(new GridLayout(5,5));
         b =new JLabel[board.getRows()][board.getRows()];
-        for(int i=0;i<b.length;i++) {
-			for(int j=0;j<b[i].length;j++) {		
-			    b[i][j] =new JLabel();
-                central.add(b[i][j]);
-		    }	
-		}
+        b[0][0]=new JLabel(new  ImageIcon(new ImageIcon("image\\x.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH)));
+        central.add(b[0][0]);
+        central.add(new JLabel(new  ImageIcon(new ImageIcon("image\\vertical.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH))));
+        b[0][1]=new JLabel(new  ImageIcon(new ImageIcon("image\\blanco.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH)));
+        central.add(b[0][1]);
+        central.add(new JLabel(new  ImageIcon(new ImageIcon("image\\vertical.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH))));
+        b[0][2]=new JLabel(new  ImageIcon(new ImageIcon("image\\cruce.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH)));
+        central.add(b[0][2]);
+
+        //----------------
+        b[1][0]=new JLabel(new  ImageIcon(new ImageIcon("image\\blanco.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH)));
+        central.add(b[1][0]);
+        central.add(new JLabel(new  ImageIcon(new ImageIcon("image\\vertical.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH))));
+        b[1][1]=new JLabel(new  ImageIcon(new ImageIcon("image\\blanco.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH)));
+        central.add(b[1][1]);
+        central.add(new JLabel(new  ImageIcon(new ImageIcon("image\\vertical.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH))));
+        b[1][2]=new JLabel(new  ImageIcon(new ImageIcon("image\\blanco.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH)));
+        central.add(b[1][2]);
+        //-----------------
+        b[2][0]=new JLabel(new  ImageIcon(new ImageIcon("image\\blanco.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH)));
+        central.add(b[2][0]);
+        central.add(new JLabel(new  ImageIcon(new ImageIcon("image\\vertical.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH))));
+        b[2][1]=new JLabel(new  ImageIcon(new ImageIcon("image\\blanco.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH)));
+        central.add(b[2][1]);
+        central.add(new JLabel(new  ImageIcon(new ImageIcon("image\\vertical.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH))));
+        b[2][2]=new JLabel(new  ImageIcon(new ImageIcon("image\\blanco.png").getImage().getScaledInstance(78, 124, Image.SCALE_SMOOTH)));
+        central.add(b[2][2]);
+        
     }
+
     void contentsDrunkBottle(Board board){
         
     }
-    void contentsBatleShips(Board board){
+    void contentsBatleShips(){
         central.setLayout(new GridLayout(1,3));
         JPanel Player1= new JPanel(new GridLayout(10,10));
         JPanel info   = new JPanel();
         JPanel Player2= new JPanel(new GridLayout(10,10));
-        b =new JLabel[board.getRows()][board.getColumns()];
-        for(int i=0;i<board.getRows();i++){
-            for(int j=0;j<board.getRows();j++){
+        b =new JLabel[this.game.getBoard().getRows()][this.game.getBoard().getColumns()];
+        for(int i=0;i<this.game.getBoard().getRows();i++){
+            for(int j=0;j<this.game.getBoard().getRows();j++){
                 b[i][j] =new JLabel();
                 Player1.add(b[i][j]);
             }
         }
-        for(int i=0;i<board.getRows();i++){
-            for(int j=board.getRows();j<board.getColumns();j++){
+        for(int i=0;i<this.game.getBoard().getRows();i++){
+            for(int j=this.game.getBoard().getRows();j<this.game.getBoard().getColumns();j++){
                 b[i][j] =new JLabel("trans");
                 Player2.add(b[i][j]);
             }
@@ -95,6 +116,13 @@ public class PrintBoard extends JFrame implements ActionListener{
         central.add(Player1);
         central.add(info);
         central.add(Player2);
+        for(int i=0;i<game.getBoard().getRows();i++){
+            for(int j=0;j<game.getBoard().getColumns();j++){
+                Image img= new ImageIcon("image\\x.png").getImage();
+                ImageIcon img2=new ImageIcon(img.getScaledInstance(78, 124, Image.SCALE_SMOOTH));
+                b[i][j].setIcon(img2);
+            }
+        }
     }
     public void actionPerformed(ActionEvent e) {
             JButton n=(JButton)e.getSource();
