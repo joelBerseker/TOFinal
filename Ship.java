24fc5307c294b6d1dba7d63e64112be2564b985f
log4x <achavezsa@unsa.edu.pe>
2019-12-18 13:00:59 -0500

    ship aleatorios

--- Ship.java
+++ Ship.java
@@ -1,15 +1,19 @@
 public class Ship{
-    private final Box InitialBox, FinalBox;
+    private Box InitialBox, FinalBox;
     private int[] indexOf_boxes;
     private int Number_of_UnmarkedBoxes;
     private char direction;
 
+    public Ship(int numberOfBoxes){
+        int random=(int)(Math.random()*100);
+        int random_aux=(int)(Math.random()*2);
+        this.Number_of_UnmarkedBoxes=numberOfBoxes;
+        fill_IndexOfBoxes(calculateIindex_and_Findex(random,random_aux,numberOfBoxes));
+    }
     public Ship(Box InitialBox,Box FinalBox) {
         this.InitialBox = InitialBox;
         this.FinalBox = FinalBox;
         Number_of_UnmarkedBoxes = numberOfBoxes();
-        if(Number_of_UnmarkedBoxes>0) fill_IndexOfBoxes();
-        setDirection();
     }
 
     public Box getInitialBox() {
@@ -58,15 +62,45 @@ public class Ship{
             return 0;
         }
     }
-    public void fill_IndexOfBoxes(){
+    public int[] calculateIindex_and_Findex(int random,int random_aux,int numberOfBoxes){
+        int []Iindex_andFindex=new int[2];
+        //1 horizontal , 0 vertical
+        if(random_aux==1){
+            if(random%10+(numberOfBoxes-1)<=9){
+                Iindex_andFindex[0]=random;
+                Iindex_andFindex[1]=random+numberOfBoxes-1;
+                return Iindex_andFindex;
+            }else{
+            //if(random%10-(numberOfBoxes-1)>=0){
+                Iindex_andFindex[0]=random-numberOfBoxes-1;
+                Iindex_andFindex[1]=random;
+                return Iindex_andFindex;
+            }
+        }else{
+            if(random-(numberOfBoxes-1)*10>=0){
+                Iindex_andFindex[0]=random-(numberOfBoxes-1)*10;
+                Iindex_andFindex[1]=random;
+                return Iindex_andFindex;
+            }else {
+            //if(random+(numberOfBoxes-1)*10<=99){
+                Iindex_andFindex[0]=random;
+                Iindex_andFindex[1]=random+(numberOfBoxes-1)*10;
+                return Iindex_andFindex;
+            }
+        }
+           
+    }
+    public void fill_IndexOfBoxes(int []Iindex_and_Findex){
+        int initialIndex=Iindex_and_Findex[0];
+        int finalIndex=Iindex_and_Findex[1];
         indexOf_boxes=new int[Number_of_UnmarkedBoxes];
-        if(FinalBox.getIndex()-InitialBox.getIndex()>=10){
-            for(int i=0;i<Number_of_UnmarkedBoxes;i+=10){
-                indexOf_boxes[i]=InitialBox.getIndex()+i;
+        if(finalIndex-initialIndex>=10){
+            for(int i=0;i<Number_of_UnmarkedBoxes;i++){
+                indexOf_boxes[i]=initialIndex+i*10;
             }
         }else{
             for(int i=0;i<Number_of_UnmarkedBoxes;i++){
-                indexOf_boxes[i]=InitialBox.getIndex()+i;
+                indexOf_boxes[i]=initialIndex+i;
             }
         }
     }
